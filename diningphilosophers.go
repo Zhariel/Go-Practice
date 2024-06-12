89b67212b5638041c9af7ef4d27869e19b5ff928
Romain Blazevic <romain.blazevic@gmail.com>
2024-06-13 00:45:41 +0200

    dining philosophers

--- diningphilosophers.go
+++ diningphilosophers.go
@@ -5,26 +5,18 @@ import (
 	"sync"
 )
 
-type Chop struct {
-	sync.Mutex
-}
-
 type Philo struct {
-	lchop, rchop *Chop
-	id           int
+	id int
 }
 
-func (p Philo) eat(perms chan []int, eatCount []int) {
+func (p Philo) eat(perms chan []int, eatCount []int, wg2 *sync.WaitGroup) {
+	defer wg2.Done()
 	perm := <-perms
 	if (p.id != perm[0] && p.id != perm[1]) || eatCount[p.id] <= 0 {
 		return
 	}
-	p.lchop.Lock()
-	p.rchop.Lock()
 	fmt.Println("Eating", p.id)
 	eatCount[p.id]--
-	p.lchop.Unlock()
-	p.rchop.Unlock()
 	fmt.Println("Finished eating", p.id)
 }
 
@@ -38,11 +30,11 @@ func sum(sli []int) int {
 
 func main() {
 	var wg sync.WaitGroup
+	var wg2 sync.WaitGroup
 
-	sticks := make([]Chop, 5)
 	philos := make([]Philo, 5)
 	for i := range philos {
-		philos[i] = Philo{&sticks[i], &sticks[(i+1)%5], i}
+		philos[i] = Philo{i}
 	}
 	eatCount := []int{3, 3, 3, 3, 3}
 	perms := make(chan []int, 5)
@@ -55,10 +47,13 @@ func main() {
 			selected := []int{x, (x + 2) % 5}
 			for i := 0; i < 5; i++ {
 				perms <- selected
-				go philos[i].eat(perms, eatCount)
+				wg2.Add(1)
+				go philos[i].eat(perms, eatCount, &wg2)
 			}
+			wg2.Wait()
 			x = (x + 1) % 5
 		}
 	}()
+
 	wg.Wait()
 }
